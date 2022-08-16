43f95e4ef6e2a588505b7256edf403a7989f01e4
Vladimir <kuznetsov.vi03@mail.ru>
2022-08-16 18:57:17 +0300

    second number

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Пожалуйста, введите целое число");
+int numberA = int.Parse(Console.ReadLine());
+if (numberA > 1000)
+
+{
+    Console.WriteLine("введите 3х значное число");
+    return;
+}
+
+int numberB = numberA % 100;
+int numberC = numberB / 10;
+Console.WriteLine("Вторая цифра введенного числа " + numberC);
