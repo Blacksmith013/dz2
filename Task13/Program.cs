530c992eadbb0fe1a342a775231337cc24e0343a
Vladimir <kuznetsov.vi03@mail.ru>
2022-08-16 19:00:02 +0300

    third number

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Пожалуйста, введите целое число");
+int numberA = int.Parse(Console.ReadLine());
+while (numberA > 1000)
+
+{
+   numberA = numberA / 10;
+}
+
+int numberB = numberA/100;
+if (numberB == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    int numberC = numberA - ((numberA/10)*10);
+    Console.WriteLine("Третья цифра введенного числа " + numberC);
+}
