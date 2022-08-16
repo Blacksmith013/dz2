56c010efd6578e3184bf78430a105ab28c2a9764
Vladimir <kuznetsov.vi03@mail.ru>
2022-08-16 19:01:00 +0300

    day of the week

---
+++ Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Пожалуйста, введите целое число");
+int numberA = int.Parse(Console.ReadLine());
+if(numberA<=5)
+{
+    Console.WriteLine("Нет");
+}
+else if(numberA <= 7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7");
+}
